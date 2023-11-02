6dc1508909de8013b33a4b001cd33de5914b1f69
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-02 06:11:16 +0200

    update global.css and src/pages/index.tsx, added some styling to the header image and text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,20 +37,23 @@ const IndexPage: React.FC<PageProps> = () => {
     <>
       <div>
         <Navbar></Navbar>
-        <CenteredTitle title="NOT FINAL PRODUCT <!>"></CenteredTitle>
+        <div className="container mx-auto p-2 bg-slate-600">
+          {/* <CenteredTitle title="NOT FINAL PRODUCT <!>"></CenteredTitle> */}
         <hr />
         <CenteredTitle title={"MC SAMUEL SHOKO"} ></CenteredTitle>
-        
 
-        <div className="flex">.
+
+        <div className="flex flex-wrap justify-evenly">
           <AvatarPortrait
-            imageUrl={"https://raw.githubusercontent.com/Stroustrups-Sentinel/readme/3dd46cdef054e046be507558bef9d0cbbafed4dc/design/maki-logo-perfectedx4800.svg"}
+            imageUrl={"https://scontent.fcpt9-1.fna.fbcdn.net/v/t39.30808-6/359749123_3389565701303522_5159761682622525303_n.png?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGCykiaP6yidBW0uyeGzG1B3QuHUFF-Td_dC4dQUX5N32jJ9Xil08TcjP0uULlVw3-xPw1S_xgrjiC1SRJoII7l&_nc_ohc=dzj-pQ234xUAX_uU5C-&_nc_zt=23&_nc_ht=scontent.fcpt9-1.fna&cb_e2o_trans=q&oh=00_AfBeWdpNpZaT6y0pAFMgsCdfFgTUo8ghYNaZ_RyEtnS2SQ&oe=65486563"}
           />
           <div>
+          <div className="mt-4"></div>
             <Paragraph text="One should, nevertheless, consider that a
               description of the basic reason of the market
               tendencies can be regarded as decidedly
               insignificant." />
+              <div className="mt-10"></div>
             <Button text="Download Resume" link={"#resume"} size={"large"} />
 
           </div>
@@ -59,7 +62,7 @@ const IndexPage: React.FC<PageProps> = () => {
         <CenteredTitle title="PROJECTS" topChevron={true} id="projects"></CenteredTitle>
         {/* <pre>{JSON.stringify(projectsData)}</pre> */}
         <div className="flex flex-wrap">
-          {projectsData.map((data) => <ProjectCard platform={data.platform} type={data.type} name={data.name} technologies={data.technologies.split(',')} description={data.description} logoUrl="https://flowbite.com/docs/images/logo.svg" imgUrl="https://flowbite.com/docs/images/logo.svg" />)}
+          {projectsData.map((data) => <ProjectCard platform={data.platform} type={data.type} name={data.name} technologies={data.technologies.split(',')} description={data.description} logoUrl="https://flowbite.com/docs/images/logo.svg" imgUrl="https://scontent.fcpt9-1.fna.fbcdn.net/v/t39.30808-6/359749123_3389565701303522_5159761682622525303_n.png?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGCykiaP6yidBW0uyeGzG1B3QuHUFF-Td_dC4dQUX5N32jJ9Xil08TcjP0uULlVw3-xPw1S_xgrjiC1SRJoII7l&_nc_ohc=dzj-pQ234xUAX_uU5C-&_nc_zt=23&_nc_ht=scontent.fcpt9-1.fna&cb_e2o_trans=q&oh=00_AfBeWdpNpZaT6y0pAFMgsCdfFgTUo8ghYNaZ_RyEtnS2SQ&oe=65486563" />)}
 
         </div>
 
@@ -82,9 +85,8 @@ const IndexPage: React.FC<PageProps> = () => {
         <CenteredTitle title="ABOUT" topChevron={true} id="about"></CenteredTitle>
         <div></div>
 
+        </div>
         <Footer></Footer>
-
-
       </div>
       <hr className="divide-x-2 divide-green-400" />
 
@@ -94,9 +96,9 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <> <title>Mc Samuel Shoko</title> 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap"/>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap"/>
-<link rel="icon" href="https://raw.githubusercontent.com/Stroustrups-Sentinel/readme/main/design/favicon.ico"/>
+export const Head: HeadFC = () => <> <title>Mc Samuel Shoko</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" />
+  <link rel="icon" href="https://raw.githubusercontent.com/Stroustrups-Sentinel/readme/main/design/favicon.ico" />
 </>
