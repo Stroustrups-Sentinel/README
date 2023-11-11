2d498579b93af85e3e8ae51fdc5d4bf470cd4064
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-11 20:45:00 +0200

    * feat(contactCard.tsx): update text size for name and tag in ContactCard component * feat(footer.tsx): add site logo, copyright text, and back to top button in Footer component * fix(navbar.tsx): comment out unnecessary label and update flex alignment in Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,12 +13,12 @@ const Navbar = ( /*breadCrumbs? : string */) => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portfolio</span>
                     </a>
                     <div className="flex md:order-2">
-                        <label className="relative inline-flex items-center cursor-pointer">
+                        {/* <label className="relative inline-flex items-center cursor-pointer">
 
                             <input type="checkbox" value="" className="sr-only peer" />
                             <div className="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Light</span>
-                        </label>
+                        </label> */}
                         <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -27,7 +27,7 @@ const Navbar = ( /*breadCrumbs? : string */) => {
                         </button>
                     </div>
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                        <ul className="font-site-sans flex flex-col font-bold text-lg p-4 md:p-0 mt-4  md:flex-row md:space-x-8 md:mt-0 md:border-0">
+                        <ul className="font-site-sans flex items-center flex-col font-bold text-lg p-4 md:p-0 mt-4  md:flex-row md:space-x-8 md:mt-0 md:border-0">
                             <li>
                                 {/* [ Breadcrumb here ] */}
 
