ed5307b4f8409f741c792bc8e077af29c4094370
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-12 11:30:16 +0200

    update navbar.tsx, removed quick-debug hack and added a todo reminder to get props from the parent

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import '../css/global.css'
 import Breadcrumb from "./breadcrumb";
 
+//TODO: add props to be used by the breadcrumb
 const Navbar = ( /*breadCrumbs? : string */) => {
     return (
         <>
@@ -30,7 +31,7 @@ const Navbar = ( /*breadCrumbs? : string */) => {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                         <ul className="font-site-sans flex items-center flex-col font-bold text-lg p-4 md:p-0 mt-4  md:flex-row md:space-x-8 md:mt-0 md:border-0">
                             <li>
-                                <Breadcrumb currentPage="Yeet"></Breadcrumb>
+                                <Breadcrumb></Breadcrumb>
                             </li>
                             <li>
                                 <a href="#projects" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 self-center" aria-current="page">Projects</a>
