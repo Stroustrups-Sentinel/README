0d38c914bf1f7d1c88ff304234c5b1dad2757ac8
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-12 10:16:33 +0200

    update breadcrumb.tsx, added a conditional to check for links to add to crumb trails

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -9,8 +9,19 @@ interface BreadcrumbProps {
 
 const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: BreadcrumbProps) => {
 
-    let currentPageElementHtml = "";
+    let currentPageElementHtml: string = "";
+    let crumbsTrail:string = "";
 
+    if (links[0].length > 0){
+        crumbsTrail = crumbsTrail + 
+        `<li>
+        <div className="flex items-center">
+
+            <svg className="w-4 h-4 mx-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="m10.18 8.05l-5.66 5.56l.87.89l5.71-5.59a1.18 1.18 0 0 0 .39-.86a1.13 1.13 0 0 0-.39-.85L5.4 1.5l-.89.88z" /></svg>
+            <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">${links.length}</a>
+        </div>
+        </li>`;
+    }
 
     if (currentPage != "") {
         currentPageElementHtml = `<li aria-current="page">
@@ -30,7 +41,7 @@ const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: Breadcrum
             <nav className="flex px-5 py-3 text-gray-700 border-2 border-grey rounded-24 bg-gray-50 dark:bg-gray-800 " aria-label="Breadcrumb">
                 <ol className="inline-flex items-center space-x-1 md:space-x-3">
                     <li className="inline-flex items-center">
-                        <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <a href={homeUrl} className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
                             {/* <svg className="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                     <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                                                 </svg> */}
@@ -38,13 +49,7 @@ const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: Breadcrum
                             Home
                         </a>
                     </li>
-                    <li>
-                        <div className="flex items-center">
-
-                            <svg className="w-4 h-4 mx-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="m10.18 8.05l-5.66 5.56l.87.89l5.71-5.59a1.18 1.18 0 0 0 .39-.86a1.13 1.13 0 0 0-.39-.85L5.4 1.5l-.89.88z" /></svg>
-                            <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Templates</a>
-                        </div>
-                    </li>
+                    {crumbsTrail}
                     {currentPageElementHtml}
                 </ol>
             </nav>
