1e4e88b39d9710e4383ec8baab370e78e683b4e6
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-12 10:32:44 +0200

    update breadcrumb.tsx, refactored current page rendering on the Breadcrumb component

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -7,14 +7,22 @@ interface BreadcrumbProps {
 
 }
 
+const currentPageElementHtml = (pageTitle: string) => (<li aria-current="page">
+    <div className="flex items-center">
+        <svg className="w-4 h-4 mx-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="m10.18 8.05l-5.66 5.56l.87.89l5.71-5.59a1.18 1.18 0 0 0 .39-.86a1.13 1.13 0 0 0-.39-.85L5.4 1.5l-.89.88z" /></svg>
+        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400 capitalize">{pageTitle}</span>
+    </div>
+</li>)
+
 const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: BreadcrumbProps) => {
 
-    let currentPageElementHtml: string = "";
-    let crumbsTrail:string = "";
 
-    if (links[0].length > 0){
-        crumbsTrail = crumbsTrail + 
-        `<li>
+
+    let crumbsTrail: string = "";
+
+    if (links[0].length > 0) {
+        crumbsTrail = crumbsTrail +
+            `<li>
         <div className="flex items-center">
 
             <svg className="w-4 h-4 mx-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="m10.18 8.05l-5.66 5.56l.87.89l5.71-5.59a1.18 1.18 0 0 0 .39-.86a1.13 1.13 0 0 0-.39-.85L5.4 1.5l-.89.88z" /></svg>
@@ -23,14 +31,7 @@ const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: Breadcrum
         </li>`;
     }
 
-    if (currentPage != "") {
-        currentPageElementHtml = `<li aria-current="page">
-    <div className="flex items-center">
-        <svg className="w-4 h-4 mx-1 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="m10.18 8.05l-5.66 5.56l.87.89l5.71-5.59a1.18 1.18 0 0 0 .39-.86a1.13 1.13 0 0 0-.39-.85L5.4 1.5l-.89.88z" /></svg>
-        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400 capitalize">${currentPage}</span>
-    </div>
-</li>`;
-    }
+
 
     return (
         <div>
@@ -50,7 +51,9 @@ const Breadcrumb = ({ links = [[]], currentPage = "", homeUrl = "/" }: Breadcrum
                         </a>
                     </li>
                     {crumbsTrail}
-                    {currentPageElementHtml}
+                    {
+                        currentPage != "" ? currentPageElementHtml(currentPage) : null
+                    }
                 </ol>
             </nav>
 
