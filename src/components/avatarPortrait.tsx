4939284112bbf41aaf56a76bee9b9c7459b8c7bb
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2023-11-17 22:06:14 +0200

    update avatarPortrait.tsx - added a border on or off prop

--- src/components/avatarPortrait.tsx
+++ src/components/avatarPortrait.tsx
@@ -7,18 +7,20 @@ interface AvatarPortraitProps {
     width?: string;
     altText?: string;
     orientation?: string;
+    border?:boolean;
 }
 
-const AvatarPortrait = ({ imageUrl, height = "469px", width = "380px", altText = "avatar-image", orientation = "portrait" }: AvatarPortraitProps) => {
+const AvatarPortrait = ({ imageUrl, height = "469px", width = "380px", altText = "avatar-image", orientation = "portrait", border=true }: AvatarPortraitProps) => {
     let newHeight: string = height;
     let classNames: string = "avatar";
     if (orientation == "square") {
         newHeight = width;
         classNames = "square-avatar";
     }
+    if (border)classNames = classNames + " border-2 ";
     return (
         <>
-            <img className={"border-2 border-gray rounded-24 " + classNames} src={imageUrl} alt={altText} height={newHeight} width={width} />
+            <img className={" border-gray rounded-24 " + classNames} src={imageUrl} alt={altText} height={newHeight} width={width} />
         </>
     );
 };
