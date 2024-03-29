dc2ff792826b5b221974cac6b75bc02785f895fa
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-03-29 03:05:49 +0200

    update tailwind.config.js, extended colors & added project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
-"./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#357266',
+        'dark-primary': "#1e4039",
+        'light-primary': '#53b3a0',
+        'accent': '#53dd6c',
+        'p-black': '#040f0f',
+        'cloud-white': '#f4f4f9',
+        'lime': '#53dd6c',
+        'dark-green': '#1e4039',
+        'border-gray': '#cac4d0'
+
+      }
+    },
   },
   plugins: [],
 }
