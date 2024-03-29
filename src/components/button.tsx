2ed41b5d9be84350d2e3cba53f44ad118299c823
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-03-29 04:45:03 +0200

    update button.tsx, refactored and added `isExternalLink` prop - added dark mode support

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,24 +6,31 @@ interface ButtonProps {
     link: string;
     size?: string;
     background?: string;
+    isExternalLink?:boolean;
     
 
 }
 
-const Button = ({ text, link, size="regular", background="default" }: ButtonProps) => {
-    let classNames : string = "rounded-24 p-2 px-4 primary-text bg-blue-400 font-bold font-site-sans big-button hover:text-gray-50 hover:shadow-md hover:bg-gray-900 transition-all hover:duration-300";
+const Button = ({ text, link, size="regular", background="default",isExternalLink=false }: ButtonProps) => {
+    let classNames : string = "rounded-24 p-2 px-4 text-p-black  font-bold font-site-sans big-button hover:text-gray-50 hover:shadow-md hover:bg-gray-900 transition-all hover:duration-300";
 
     if (background == "black") {
-        classNames = classNames + " black-bg lime-text";
+        classNames = classNames + " bg-p-black text-lime dark:text-p-black dark:bg-cloud-white";
     }
     else{
     classNames = classNames + " bg-accent ";}
 
-    // if (size == "large"){ classNames = classNames +" big-button"; }
 
     return (
-        <a href={link}>
-            <button className={classNames} type="button">{text}</button>
+        <a href={link} target= {(isExternalLink ? "_blank":"")}>
+            <button className={classNames} type="button">
+                {
+                    isExternalLink ? <span className="flex">
+                    <p>{text}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="M14.21 1.5H10v1.25h3.08L7.9 7.21l.82 1l5.53-4.77V7h1.25V2.79a1.29 1.29 0 0 0-1.29-1.29"/><path fill="currentColor" d="M12.25 13.25H1.75v-8.5H7.5V3.5h-6a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4h-1.25z"/></svg>
+                </span> : text
+                }
+            </button>
         </a>
     );
 }
