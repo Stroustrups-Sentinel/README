2a676925437174712d26da024c99aad541351c6b
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-04-17 18:17:31 +0200

    update technologyIcon.tsx, added `invertColor` to props

--- src/components/technologyIcon.tsx
+++ src/components/technologyIcon.tsx
@@ -5,11 +5,13 @@ interface TechnologyIconProps{
     src: string;
     alt: string;
     link: string;
+    invertColor?:boolean;
 }
 
-const TechnologyIcon = ({ src, alt, link}:TechnologyIconProps) => {
+const TechnologyIcon = ({ src, alt, link, invertColor=false}:TechnologyIconProps) => {
+    const imgStyles:string = (invertColor? 'dark:invert' : '');
     return (
-        <a className="m-2" href={link} target="_blank" title={"click to visit "+alt}><img className="tech-icon" src={src} alt={alt} /></a>
+        <a className="m-2" href={link} target="_blank" title={"click to visit "+alt}><img className={"tech-icon "+imgStyles} src={src} alt={alt} /></a>
     );
 };
 
