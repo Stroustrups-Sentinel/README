39d6e60b20abd124e3ee24fb44837b345c7150f3
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-04-21 19:52:11 +0200

    update 404.tsx, added siteUrl checking function

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,11 @@ import { Link, HeadFC, PageProps } from "gatsby"
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
 
+import {getSiteUrl } from "../components/projectPage";
 
 
 const NotFoundPage: React.FC<PageProps> = () => {
-
+  const siteUrl: string = getSiteUrl();
 
   return (
     <>
@@ -32,7 +33,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
               <br />
               <div className="flex place-items-center font-bold">
 
-                <Link to="/"><button className="flex place-items-center py-2 pl-3 pr-4 primary-text rounded-24 dark-green-text font-site-mono  text-white navbar-gradient hover:outline hover:outline-black dark:hover:outline-gray-200">
+                <Link to={siteUrl}><button className="flex place-items-center py-2 pl-3 pr-4 primary-text rounded-24 dark-green-text font-site-mono  text-white navbar-gradient hover:outline hover:outline-black dark:hover:outline-gray-200">
                   <svg className="h-5 w-5 inline-flex" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="currentColor" d="M15.5 14.69h-1.25V7.78a.62.62 0 0 0-.25-.47L8.4 2.7a.65.65 0 0 0-.81 0L2 7.31a.62.62 0 0 0-.22.47v6.91H.5V7.78a1.87 1.87 0 0 1 .68-1.44l5.62-4.6a1.88 1.88 0 0 1 2.39 0l5.63 4.6a1.87 1.87 0 0 1 .68 1.44z" /><path fill="currentColor" d="M11.05 12.11H9.8A1.72 1.72 0 0 0 8 10.49a1.72 1.72 0 0 0-1.8 1.62H5a3 3 0 0 1 3-2.87a3 3 0 0 1 3.05 2.87m-6.1 0H6.2v2.58H4.95zm4.85 0h1.25v2.58H9.8z" /></svg> &nbsp;
 
                   Take me Home</button></Link>
