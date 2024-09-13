e07411111aa774fb1cc515ce04430b97877d8b3c
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-09-13 19:18:32 +0200

    update gatsby-config.ts - fixed siteUrl

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://stroustrups-sentinel.github.io/readme/`,
+    siteUrl: `https://mcsamuelshoko.github.io/readme/`,
     description: "Mc Samuel Shoko | Software Engineering portfolio"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
